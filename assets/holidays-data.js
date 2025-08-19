90e1f97739dee253b5ad1b20644b19f267be0e78
turk-mus <as3aralywmsaudi@gmail.com>
2025-08-19 15:57:56 +0300

    stud

---
+++ assets/holidays-data.js
@@ -0,0 +1,8 @@
+// assets/holidays-data.js
+window.HOLIDAYS_UPDATED_AT = "2025-08-19T12:00:00Z";
+
+window.HOLIDAYS = [
+  { title: "إجازة اليوم الوطني",    start: "2025-09-23", end: "2025-09-23" },
+  { title: "إجازة نهاية فصل دراسي", start: "2025-12-04", end: "2025-12-06" },
+  { title: "إجازة منتصف الفصل",     start: "2026-02-05", end: "2026-02-07" }
+];
